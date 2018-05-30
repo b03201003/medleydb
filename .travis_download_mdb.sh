e1a86ee92811c58b431aa3517dbb1a3bbc8dba09
Rachel Bittner <rachelbittner@spotify.com>
2018-05-30 17:58:09 -0400

    change medleydb sample download location

--- .travis_download_mdb.sh
+++ .travis_download_mdb.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/data/MedleyDB_sample" ]; then
     mkdir -p $HOME/data
-    wget http://marl.smusic.nyu.edu/medleydb_webfiles/MedleyDB_sample.tar.gz -O $HOME/data/MedleyDB_sample.tar.gz
+    wget https://googledrive.com/host/1dpmF7DiiLrZ4xUHaYQB4rn4v8UDsUBve -O $HOME/data/MedleyDB_sample.tar.gz
     gzip -d $HOME/data/MedleyDB_sample.tar.gz
     tar xvf $HOME/data/MedleyDB_sample.tar -C $HOME/data/
 fi
