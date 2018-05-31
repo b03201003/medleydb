40a1893b4cf240d33623b0b120c144ec46b0cb49
Rachel Bittner <rachelbittner@spotify.com>
2018-05-31 15:06:18 -0400

    fix filepath

--- .travis_download_mdb.sh
+++ .travis_download_mdb.sh
@@ -2,9 +2,7 @@
 
 if [ ! -d "$HOME/data/MedleyDB_sample" ]; then
     mkdir -p $HOME/data
-    echo $PWD
-    ls lfs/*
-    cp lfs/MedleyDB_sample.tar.gz $HOME/data/MedleyDB_sample.tar.gz
+    cp lfs/MedleyDB_Sample.tar.gz $HOME/data/MedleyDB_sample.tar.gz
     gzip -d $HOME/data/MedleyDB_sample.tar.gz
     tar xvf $HOME/data/MedleyDB_sample.tar -C $HOME/data/
 fi
