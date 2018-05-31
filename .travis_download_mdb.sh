e47505c44a586e2fd75a2d5e4e909f88b1967477
Rachel Bittner <rachelbittner@spotify.com>
2018-05-31 15:13:51 -0400

    debug

--- .travis_download_mdb.sh
+++ .travis_download_mdb.sh
@@ -3,6 +3,7 @@
 if [ ! -d "$HOME/data/MedleyDB_sample" ]; then
     mkdir -p $HOME/data
     cp lfs/MedleyDB_Sample.tar.gz $HOME/data/MedleyDB_sample.tar.gz
+    ls $HOME/data/*
     gzip -d $HOME/data/MedleyDB_sample.tar.gz
     tar xvf $HOME/data/MedleyDB_sample.tar -C $HOME/data/
 fi
