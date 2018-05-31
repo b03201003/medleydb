5d750b74926a84ad6ea21951a22aa86d5a752da7
Rachel Bittner <rachelbittner@spotify.com>
2018-05-31 14:55:42 -0400

    Copy local sample instead of pulling from internet

--- .travis_download_mdb.sh
+++ .travis_download_mdb.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/data/MedleyDB_sample" ]; then
     mkdir -p $HOME/data
-    wget https://googledrive.com/host/1dpmF7DiiLrZ4xUHaYQB4rn4v8UDsUBve -O $HOME/data/MedleyDB_sample.tar.gz
+    cp lfs/MedleyDB_sample.tar.gz $HOME/data/MedleyDB_sample.tar.gz
     gzip -d $HOME/data/MedleyDB_sample.tar.gz
     tar xvf $HOME/data/MedleyDB_sample.tar -C $HOME/data/
 fi
